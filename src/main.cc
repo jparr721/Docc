ae0e02e2dda37365ee73c46535638054d5a7693e
Jarred Parr <jparr721@gmail.com>
2019-08-03 08:48:07 -0600

    change function name

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <memory>
+#include <unistd.h>
 #include <X11/Xlib.h>
 #include <X11/X.h>
 #include <X11/Xutil.h>
 
-XKeyEvent createKeyEvent(Display *display, Window &win,
+XKeyEvent create_key_event(Display *display, Window &win,
                          Window &root, bool press,
                          int keycode, int modifiers) {
   XKeyEvent event;
@@ -18,7 +19,8 @@ XKeyEvent createKeyEvent(Display *display, Window &win,
   event.x_root = 1;
   event.y_root = 1;
   event.same_screen = True;
-  event.keycode = 0xff1b;
+  /* event.keycode = 0xff1b; */
+  event.keycode = XKeysymToKeycode(display, keycode);
   event.state = modifiers;
   event.type = KeyPress;
 
@@ -28,15 +30,11 @@ XKeyEvent createKeyEvent(Display *display, Window &win,
 int main(int argc, char** argv) {
   Display *dpy = XOpenDisplay(0);
   Screen* s = DefaultScreenOfDisplay(dpy);
-  XKeyEvent xke;
-  xke.display = dpy;
-  xke.keycode = 0xff1b;
   Window root_window;
 
   auto height = s->height;
   auto width = s->width;
-  std::cout << height << std::endl;
-  std::cout << width << std::endl;
+
   auto center_x = height / 2;
   auto center_y = height /2;
 
@@ -44,6 +42,21 @@ int main(int argc, char** argv) {
   XSelectInput(dpy, root_window, KeyReleaseMask);
 
   XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, center_x, center_y);
+  usleep(100000);
+
+  for (int i = 0; i < 50; ++i) {
+    Window focus_window;
+
+    XKeyEvent event = create_key_event(dpy, focus_window, root_window, true, 0xff0d, 0);
+    XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
+    std::cout << "Key pressed" << std::endl;
+
+    usleep(100000);
+
+    event = create_key_event(dpy, focus_window, root_window, false, 0xff0d, 0);
+    XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent *)&event);
+    std::cout << "Key unpressed" << std::endl;
+  }
 
   XFlush(dpy);
 
