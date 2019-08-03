c3bf9d42067fda87b970f43b3d059b429be8ec56
Jarred Parr <jparr721@gmail.com>
2019-08-02 21:39:27 -0600

    now it centers

--- src/main.cc
+++ src/main.cc
@@ -1,17 +1,25 @@
+#include <iostream>
+#include <memory>
 #include <X11/Xlib.h>
 #include <X11/X.h>
 #include <X11/Xutil.h>
-#include <memory>
 
 int main(int argc, char** argv) {
-  Display *dpy;
+  Display *dpy = XOpenDisplay(0);
+  Screen* s = DefaultScreenOfDisplay(dpy);
   Window root_window;
 
-  dpy = XOpenDisplay(0);
+  auto height = s->height;
+  auto width = s->width;
+  std::cout << height << std::endl;
+  std::cout << width << std::endl;
+  auto center_x = height / 2;
+  auto center_y = height /2;
+
   root_window = XRootWindow(dpy, 0);
   XSelectInput(dpy, root_window, KeyReleaseMask);
 
-  XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, 100, 100);
+  XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, center_x, center_y);
 
   XFlush(dpy);
 
